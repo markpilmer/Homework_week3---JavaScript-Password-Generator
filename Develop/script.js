0308b1ede12248fa03bc3c82c6454f786e5e68b1
Pilly <pilly78@gmail.com>
2020-09-11 19:27:33 +0800

    Psuedo code for initail inputs

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+// PseudoCode
+// 1. Prompt user for number bewteen 8 -128
+    // validate input
+// 2. confirm user for lowercase
+// 3. confirm user for uppercase
+// 4. confirm user for numeric
+// 5. confirm user for special characters
+
+// 6. disply the crieria that was chosen
+    // confirm criteria is correct
+
+// 7. if user confirms criteria correct generate password
+// 8. If user clicks cancel prompt for criteria again
+
 // Write password to the #password input
 function writePassword() {
 
