4937a31cef7850bb842a6215d1627f05e8e6a9e8
markpilmer <67323873+markpilmer@users.noreply.github.com>
2020-09-11 18:57:48 +0800

    Initial dump of files

---
+++ Develop/script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+
+
+
+
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordTextbox.value = password;
+
+}
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
